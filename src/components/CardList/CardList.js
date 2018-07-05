6b27118433a074e6f7168f349f183d6e8f6c1fc7
Chris Demaio <chris@Chriss-MBP.fios-router.home>
2018-07-05 16:52:34 -0400

    updated minor things

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Card from '../Card/Card';
 
 const CardList = (props) => {
+	
 	if(!props.forecast){
 		return <div> Loading ... </div>;
 	}
 
-
 	const weatherCards = props.forecast.map((item, i) => {
-
 		return <Card 
 			key={i}
 			id={i}
 			forecast={props.forecast}
+			wind={props.wind}
 			/>
 	});
 
