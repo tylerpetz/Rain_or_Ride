ffbdd957cd2aa68d53a0bdf5fc2278591b876e6c
Tyler Petz <tylerpetz@gmail.com>
2018-07-09 21:27:51 -0400

    added gitignore

--- node_modules/react-siema/src/lib/utils/transformProperty.js
+++
@@ -1,7 +0,0 @@
-export default (() => {
-    const { transform } = document.documentElement.style;
-    if (typeof transform === 'string') {
-        return 'transform';
-    }
-    return 'WebkitTransform';
-})();
